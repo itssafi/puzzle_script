9c15af2a847b9305d7bbc91f90d0f9bd239a8ef4
itssafi <jsafiulla@zeomega.com>
2015-03-19 12:14:32 +0530

    Modified at office

--- puzzle_script.py
+++ puzzle_script.py
@@ -5,17 +5,15 @@ class RestaurantPuzzle(object):
     """
     Class for solving Jurgensville Restaurant Puzzle
     """
-    def get_restaurant(self, kwargs):
+    def read_csv_file(self, kwargs):
         """
-        It will return restaurant id and minimum cost of dinner
+        Reading csv file reacord and put into a dictionary
         """
-        # Reading csv file reacord and put into a dictionary
         restaurant_details = {}
         csv_file = kwargs['csv_file']
         with open(csv_file) as file_obj:
             for line in file_obj:
                 line = line.rstrip()
-                #import pdb; pdb.set_trace()
                 list_items = line.split(', ')
                 restaurant_id = list_items.pop(0)
                 cost = list_items.pop(0)
@@ -26,19 +24,25 @@ class RestaurantPuzzle(object):
                 else:
                     restaurant_details[restaurant_id].append(cost_and_item)
 
+        return restaurant_details
+
+    def get_restaurant_id_and_min_cost(self, kwargs):
+        """
+        It will return restaurant id and minimum cost of dinner
+        """
         flag = 0
         rest_no = 0
         total_cost = 0
         cost_list = []
         id_and_cost = {}
         no_of_item = len(kwargs['item_list'])
+        restaurant_details = self.read_csv_file(kwargs)
+
         for key in restaurant_details:
             items_list = []
             cost_list = []
             for search_item in restaurant_details[key]:
                 for cost, item in search_item.iteritems():
-                    import pdb; pdb.set_trace()
-                    print item
                     if kwargs['item_list'] == item:
                         id_and_cost[key] = cost
                         rest_no += 1
@@ -59,9 +63,12 @@ class RestaurantPuzzle(object):
                 flag = 1
 
         if rest_no > 1:
-            print min(id_and_cost.values())
+            restaurant_id =  min(id_and_cost, key=id_and_cost.get)
+            print restaurant_id, min(id_and_cost.values())
+
         elif rest_no == 1:
             print id_and_cost.keys()[0], id_and_cost.values()[0]
+
         if flag == 0:
             print 'Nil'
 
@@ -72,4 +79,4 @@ if __name__ == '__main__':
     del sys.argv[0]
     param_dict['item_list'] = sys.argv
     restn = RestaurantPuzzle()
-    restn.get_restaurant(param_dict)
+    restn.get_restaurant_id_and_min_cost(param_dict)
