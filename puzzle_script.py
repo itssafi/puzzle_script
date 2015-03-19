4c9bc9320cc52a21e6cae8433f09304397b8827c
itssafi <jsafiulla@zeomega.com>
2015-03-20 01:12:21 +0530

    Fixed code

--- puzzle_script.py
+++ puzzle_script.py
@@ -18,12 +18,12 @@ class RestaurantPuzzle(object):
                 restaurant_id = list_items.pop(0)
                 if list_items:
                     cost = list_items.pop(0)
-                dinner_items = list(list_items)
-                cost_and_item = {float(cost) : dinner_items}
-                if restaurant_id not in self.restaurant_details.keys():
-                    self.restaurant_details[restaurant_id] = [cost_and_item]
-                else:
-                    self.restaurant_details[restaurant_id].append(cost_and_item)
+                    dinner_items = list(list_items)
+                    dinner_items.insert(0, float(cost))
+                    if restaurant_id not in self.restaurant_details.keys():
+                        self.restaurant_details[restaurant_id] = [dinner_items]
+                    else:
+                        self.restaurant_details[restaurant_id].append(dinner_items)
 
         return self.restaurant_details
 
@@ -35,61 +35,50 @@ class RestaurantPuzzle(object):
         self.rest_no = 0
         self.cost_list = []
         self.id_and_cost = {}
-        self.no_of_item = len(kwargs['item_list'])
+        self.search_items = kwargs['item_list']
 
-        if self.no_of_item < 1:
+        if len(self.search_items) < 1:
             print 'Please pass search item...!'
             return
 
         self.restaurant_details = self.read_csv_file(kwargs)
 
-        for key in self.restaurant_details:
-            items_list = []
-            cost_list = []
-            for search_item in self.restaurant_details[key]:
-                temp_list = []
-                cost = 0
-                for cost, item in search_item.iteritems():
-                    temp_list.extend(item)
-                    cost = cost
+        for key in self.restaurant_details.keys():
+            temp_list = []
+            cost = []
+            for items in self.restaurant_details[key]:
+                if set(items).issuperset(self.search_items):
+                    self.set_values(key, items[0])
 
-                    # Checking for single item for single cost
-                    if kwargs['item_list'] == item:
-                        self.set_values(key, cost)
-                        continue
+                else:
+                    temp_list.extend(items)
+                    cost.append(items[0])
 
-                    # Checking for items for single cost present in each restaurant  
-                    elif sorted(kwargs['item_list']) == sorted(item):
-                        self.set_values(key, cost)
-                        continue
-
-                # Checking for multiple items in each restaurant
-                if set(temp_list).issuperset(kwargs['item_list']):
-                    self.set_values(key, cost)
-                    continue
-
-                # Checking for all items in each restaurant
-                if len(kwargs['item_list']) > 1:
-                    items_list.extend(search_item.values()[0])
-                    cost_list.extend(search_item.keys())
-            if set(items_list).issuperset(kwargs['item_list']):
-                self.set_values(key, sum(cost_list))
+            if set(temp_list).issuperset(self.search_items):
+                self.set_values(key, sum(cost))
 
         # Checking if more than one restaurant found for search item/items
         if self.rest_no > 1:
-            restaurant_id =  min(self.id_and_cost, key=self.id_and_cost.get)
-            print restaurant_id, min(self.id_and_cost.values())
+            value_list = {}
+            for value in self.id_and_cost:
+                value_list[value] = min(self.id_and_cost[value])
+
+            restaurant_id =  min(value_list, key=value_list.get)
+            print restaurant_id, min(value_list.values())
 
         # Checking for one restaurant found for search item/items
         elif self.rest_no == 1:
-            print self.id_and_cost.keys()[0], self.id_and_cost.values()[0]
+            print self.id_and_cost.keys()[0], min(self.id_and_cost.values()[0])
 
         # Checking if search item/items not found 
         if self.flag == 0:
             print 'Nil'
 
     def set_values(self, rest_id, cost):
-        self.id_and_cost[rest_id] = cost
+        if rest_id not in self.id_and_cost.keys():
+            self.id_and_cost[rest_id] = [cost]
+        else:
+            self.id_and_cost[rest_id].append(cost)
         self.rest_no += 1
         self.flag = 1
 
