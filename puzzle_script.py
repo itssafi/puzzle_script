325947276dd3a3fee35fc52a1d63cc36a701e203
itssafi <jsafiulla@zeomega.com>
2015-03-19 13:15:44 +0530

    Final code

--- puzzle_script.py
+++ puzzle_script.py
@@ -32,51 +32,76 @@ class RestaurantPuzzle(object):
         """
         flag = 0
         rest_no = 0
-        total_cost = 0
         cost_list = []
         id_and_cost = {}
         no_of_item = len(kwargs['item_list'])
+
+        if no_of_item < 1:
+            print 'Please pass search item...!'
+            return
         restaurant_details = self.read_csv_file(kwargs)
 
         for key in restaurant_details:
             items_list = []
             cost_list = []
             for search_item in restaurant_details[key]:
+                temp_list = []
+                cost = 0
                 for cost, item in search_item.iteritems():
+                    temp_list.extend(item)
+                    cost = cost
+
+                    # Checking for single item for single cost
                     if kwargs['item_list'] == item:
                         id_and_cost[key] = cost
                         rest_no += 1
                         flag = 1
                         continue
+
+                    # Checking for items for single cost present in each restaurent  
                     elif sorted(kwargs['item_list']) == sorted(item):
                         id_and_cost[key] = cost
                         rest_no += 1
                         flag = 1
                         continue
+
+                # Checking for multiple items in each restaurent
+                if set(temp_list).issuperset(kwargs['item_list']):
+                    id_and_cost[key] = cost
+                    rest_no += 1
+                    flag = 1
+                    continue
+
+                # Checking for all items in each restaurent
                 if len(kwargs['item_list']) > 1:
                     items_list.extend(search_item.values()[0])
                     cost_list.extend(search_item.keys())
-
             if set(items_list).issuperset(kwargs['item_list']):
                 id_and_cost[key] = sum(cost_list)
                 rest_no += 1
                 flag = 1
 
+        # Checking if more than one restaurent found for search item/items
         if rest_no > 1:
             restaurant_id =  min(id_and_cost, key=id_and_cost.get)
             print restaurant_id, min(id_and_cost.values())
 
+        # Checking for one restaurent found for search item/items
         elif rest_no == 1:
             print id_and_cost.keys()[0], id_and_cost.values()[0]
 
+        # Checking if search item/items not found 
         if flag == 0:
             print 'Nil'
 
 
 if __name__ == '__main__':
-    param_dict = {'csv_file' : sys.argv[1], 'item_list' : None}
-    del sys.argv[0]
-    del sys.argv[0]
-    param_dict['item_list'] = sys.argv
-    restn = RestaurantPuzzle()
-    restn.get_restaurant_id_and_min_cost(param_dict)
+    try:
+        param_dict = {'csv_file' : sys.argv[1], 'item_list' : None}
+        del sys.argv[0]
+        del sys.argv[0]
+        param_dict['item_list'] = sys.argv
+        restn = RestaurantPuzzle()
+        restn.get_restaurant_id_and_min_cost(param_dict)
+    except Exception, err:
+        print "Error occured due to '%s'" %err
